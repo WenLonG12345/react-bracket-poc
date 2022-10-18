1454cce4f5665884043734085ec87418590617f7
Teo Wen Long <teowenlong0316@gmail.com>
2022-10-18 09:25:23 +0800

    feat: add seed header

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,7 @@ function App() {
         {
           id: 1,
           date: new Date().toDateString(),
+          title: "Dec 4, 2022 - Thu 6:00 PM",
           teams: [
             { name: "Team A", avatar: GERMANY_LOGO, score: 1 },
             { name: "Team B", avatar: JAPAN_LOGO, score: 2 },
@@ -33,6 +34,7 @@ function App() {
         {
           id: 2,
           date: new Date().toDateString(),
+          title: "Dec 4, 2022 - Thu 6:00 PM",
           teams: [
             { name: "Team C", avatar: GERMANY_LOGO, score: 1 },
             { name: "Team D", avatar: JAPAN_LOGO, score: 3 },
@@ -41,6 +43,7 @@ function App() {
         {
           id: 3,
           date: new Date().toDateString(),
+          title: "Dec 4, 2022 - Thu 6:00 PM",
           teams: [
             { name: "Team E", avatar: GERMANY_LOGO, score: 1 },
             { name: "Team F", avatar: JAPAN_LOGO, score: 4 },
@@ -49,6 +52,7 @@ function App() {
         {
           id: 4,
           date: new Date().toDateString(),
+          title: "Dec 4, 2022 - Thu 6:00 PM",
           teams: [
             { name: "Team G", avatar: GERMANY_LOGO, score: 4 },
             { name: "Team H", avatar: JAPAN_LOGO, score: 1 },
@@ -57,6 +61,7 @@ function App() {
         {
           id: 5,
           date: new Date().toDateString(),
+          title: "Dec 4, 2022 - Thu 6:00 PM",
           teams: [
             { name: "Team I", avatar: GERMANY_LOGO, score: 1 },
             { name: "Team J", avatar: JAPAN_LOGO, score: 5 },
@@ -65,6 +70,7 @@ function App() {
         {
           id: 6,
           date: new Date().toDateString(),
+          title: "Dec 4, 2022 - Thu 6:00 PM",
           teams: [
             { name: "Team K", avatar: GERMANY_LOGO, score: 3 },
             { name: "Team L", avatar: JAPAN_LOGO, score: 4 },
@@ -73,6 +79,7 @@ function App() {
         {
           id: 7,
           date: new Date().toDateString(),
+          title: "Dec 4, 2022 - Thu 6:00 PM",
           teams: [
             { name: "Team M", avatar: GERMANY_LOGO, score: 5 },
             { name: "Team N", avatar: JAPAN_LOGO, score: 1 },
@@ -81,6 +88,7 @@ function App() {
         {
           id: 8,
           date: new Date().toDateString(),
+          title: "Dec 4, 2022 - Thu 6:00 PM",
           teams: [
             { name: "Team C", avatar: GERMANY_LOGO, score: 1 },
             { name: "Team D", avatar: JAPAN_LOGO, score: 1 },
@@ -94,6 +102,7 @@ function App() {
         {
           id: 9,
           date: new Date().toDateString(),
+          title: "Dec 4, 2022 - Thu 6:00 PM",
           teams: [
             { name: "Team A", avatar: GERMANY_LOGO, score: 1 },
             { name: "Team C", avatar: MALAYSIA_LOGO, score: 1 },
@@ -102,6 +111,7 @@ function App() {
         {
           id: 10,
           date: new Date().toDateString(),
+          title: "Dec 4, 2022 - Thu 6:00 PM",
           teams: [
             { name: "Team B", avatar: MALAYSIA_LOGO, score: 1 },
             { name: "Team E", avatar: GERMANY_LOGO, score: 1 },
@@ -110,6 +120,7 @@ function App() {
         {
           id: 11,
           date: new Date().toDateString(),
+          title: "Dec 4, 2022 - Thu 6:00 PM",
           teams: [
             { name: "Team D", avatar: GERMANY_LOGO, score: 2 },
             { name: "Team F", avatar: MALAYSIA_LOGO, score: 7 },
@@ -118,6 +129,7 @@ function App() {
         {
           id: 12,
           date: new Date().toDateString(),
+          title: "Dec 4, 2022 - Thu 6:00 PM",
           teams: [
             { name: "Team G", avatar: MALAYSIA_LOGO, score: 1 },
             { name: "Team H", avatar: GERMANY_LOGO, score: 8 },
@@ -131,6 +143,7 @@ function App() {
         {
           id: 13,
           date: new Date().toDateString(),
+          title: "Dec 4, 2022 - Thu 6:00 PM",
           teams: [
             { name: "Team A", avatar: JAPAN_LOGO, score: 10 },
             { name: "Team C", avatar: MALAYSIA_LOGO, score: 1 },
@@ -139,6 +152,7 @@ function App() {
         {
           id: 14,
           date: new Date().toDateString(),
+          title: "Dec 4, 2022 - Thu 6:00 PM",
           teams: [
             { name: "Team A", avatar: MALAYSIA_LOGO, score: 1 },
             { name: "Team C", avatar: JAPAN_LOGO, score: 9 },
@@ -152,6 +166,7 @@ function App() {
         {
           id: 5,
           date: new Date().toDateString(),
+          title: "Dec 4, 2022 - Thu 6:00 PM",
           teams: [
             { name: "Team A", avatar: GERMANY_LOGO, score: 5 },
             { name: "Team C", avatar: JAPAN_LOGO, score: 1 },
@@ -161,64 +176,68 @@ function App() {
     },
   ];
 
-  const CustomSeed = ({
-    seed,
-    roundIndex,
-    seedIndex,
-  }: IRenderSeedProps) => {
-    // breakpoint passed to Bracket component
-    // to check if mobile view is triggered or not
-
-    // mobileBreakpoint is required to be passed down to a seed
+  const CustomSeed = ({ seed, roundIndex, seedIndex }: IRenderSeedProps) => {
+    console.log({ seed });
     return (
       <Seed style={{ fontSize: 12 }}>
         <SeedItem style={{ backgroundColor: "white", color: "black" }}>
-          <SeedTeam style={{ justifyContent: "space-between" }}>
-            <div
-              style={{
-                display: "flex",
-                border: "1px solid #000",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              <img src={seed.teams[0]?.avatar} width="50" />
-            </div>
-            <div
-              style={{
-                justifyContent: "space-between",
-                display: "flex",
-                flex: 1,
-                padding: "0px 10px",
-              }}
-            >
-              <div>{seed.teams[0]?.name || "NO TEAM "}</div>
-              <div>{seed.teams[0]?.score}</div>
-            </div>
-          </SeedTeam>
-          <SeedTeam>
-            <div
-              style={{
-                display: "flex",
-                border: "1px solid #000",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              <img src={seed.teams[1]?.avatar} width="50" />
-            </div>
-            <div
-              style={{
-                justifyContent: "space-between",
-                display: "flex",
-                flex: 1,
-                padding: "0px 10px",
-              }}
-            >
-              <div>{seed.teams[1]?.name || "NO TEAM "}</div>
-              <div>{seed.teams[1]?.score}</div>
-            </div>
-          </SeedTeam>
+          <div
+            style={{
+              backgroundColor: "#f5f5f5",
+              padding: "10px 0px",
+              fontWeight: "bold",
+            }}
+          >
+            {seed.title}
+          </div>
+          <div style={{ margin: "10px 0px" }}>
+            <SeedTeam>
+              <div
+                style={{
+                  display: "flex",
+                  border: "1px solid #000",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <img src={seed.teams[0]?.avatar} width="50" />
+              </div>
+              <div
+                style={{
+                  justifyContent: "space-between",
+                  display: "flex",
+                  flex: 1,
+                  padding: "0px 10px",
+                }}
+              >
+                <div>{seed.teams[0]?.name || "NO TEAM "}</div>
+                <div>{seed.teams[0]?.score}</div>
+              </div>
+            </SeedTeam>
+            <SeedTeam>
+              <div
+                style={{
+                  display: "flex",
+                  border: "1px solid #000",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <img src={seed.teams[1]?.avatar} width="50" />
+              </div>
+              <div
+                style={{
+                  justifyContent: "space-between",
+                  display: "flex",
+                  flex: 1,
+                  padding: "0px 10px",
+                }}
+              >
+                <div>{seed.teams[1]?.name || "NO TEAM "}</div>
+                <div>{seed.teams[1]?.score}</div>
+              </div>
+            </SeedTeam>
+          </div>
         </SeedItem>
       </Seed>
     );
--- src/TournamentBracket/types/Seed.ts
+++ src/TournamentBracket/types/Seed.ts
@@ -4,6 +4,7 @@ export type ISeedProps = {
   id: number | string;
   teams: Array<{ name?: string; [key: string]: any }>;
   date?: string;
+  title?: string;
   [key: string]: any;
 };
 
